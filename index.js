b39aade084d10e8d7721c9a7be67c2c57bb507db
oskar <oskar.bahr.lg@gmail.com>
2024-07-05 10:49:45 +0200

    Added bootstrap

--- index.js
+++ index.js
@@ -102,6 +102,7 @@ const app = express()
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 
